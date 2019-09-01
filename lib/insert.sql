4765098a03c6b5e6a42b959a7f19e83279b57cd5
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-01 19:06:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'black', 'angry', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'female', 'brown', 'calm', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'male', 'white', 'gentle', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'female', 'black', 'calm', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Melissa', 1, 'male', 'black', 'angry', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'female', 'brown', 'angry', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'male', 'white', 'gentle', 1);
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'black', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'male', 'white', 'gentle', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'female', 'black', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 1, 'male', 'black', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'female', 'brown', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'male', 'white', 'gentle', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, 0);
 
 ```
 
