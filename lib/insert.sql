f67d095b830a6258b45c2edaa9420f98e89e7529
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-01 19:01:20 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES (
+  'Mr. Chocolate', 3, 'male', 'black', 'angry', 1);
+
+```
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+gender TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER
+
+
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
+unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+```
