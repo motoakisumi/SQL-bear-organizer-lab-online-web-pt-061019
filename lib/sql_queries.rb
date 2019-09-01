e05cd2546936f55656e3e20ba78f04b2f26d6ac8
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-01 19:40:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT TOP 1 name FROM bears GROUP BY color ORDER BY COUNT(color) DESC"
+  "SELECT MAX(SELECT COUNT(color) from bears) from bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
