8d9b744b2b4947fd3c0cc36b05ef4e70e8da3d21
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-01 19:46:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Mr. Chocolate', 3, 'male', 'black', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Rowdy', 2, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Tabitha', 6, 'female', 'white', 'gentle', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Sergeant Brown', 5, 'male', 'black', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Melissa', 13, 'female', 'black', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Grinch', 3, 'male', 'brown', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Wendy', 6, 'female', 'white', 'gentle', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, NULL, NULL, NULL, NULL, 0);
