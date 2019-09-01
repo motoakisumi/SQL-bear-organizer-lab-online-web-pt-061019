16578f56e046372bcf4d8cc002801c5eae51d802
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-01 19:43:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperaments) FROM bears WHERE
+  "SELECT COUNT(temperament) FROM bears WHERE
     temeraments = 'goofy'"
 end
 
